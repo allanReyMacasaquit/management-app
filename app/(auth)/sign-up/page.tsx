4256616659f6d439e94816cf07564dc3a46a8930
allan <allanrey.macasaquit@gmail.com>
2024-10-14 23:27:02 -0400

    initial create for auth API

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import SignUpCard from '@/features/auth/sign-up-card';
+import SignUpCard from '@/features/auth/components/sign-up-card';
 
 function SignUpPage() {
 	return <SignUpCard />;
